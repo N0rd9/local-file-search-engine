a18dcd7d6ee0f6b28733cc6bb2096fe032323f6a
N0rd9 <jpbcfontes@gmail.com>
2026-04-28 17:11:56 -0300

    Update search_engine.py

--- search_engine.py
+++ search_engine.py
@@ -1,1 +1,24 @@
+import difflib
+from database import get_all_files
 
+def search(query, file_type="All"):
+    query = query.lower()
+    results = []
+
+    for name, path, ext in get_all_files():
+
+        if file_type != "All" and ext != file_type:
+            continue
+
+        if query == name:
+            score = 100
+        elif query in name:
+            score = 70 + name.count(query)
+        else:
+            score = int(difflib.SequenceMatcher(None, query, name).ratio() * 50)
+
+        if score > 20:
+            results.append((score, path))
+
+    results.sort(reverse=True)
+    return [path for score, path in results[:100]]
