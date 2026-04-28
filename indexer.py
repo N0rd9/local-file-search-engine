7b91c337f86b416c379599e44ba6a00b250caff9
N0rd9 <jpbcfontes@gmail.com>
2026-04-28 17:11:45 -0300

    Add folder indexing functionality

--- indexer.py
+++ indexer.py
@@ -1,1 +1,10 @@
+import os
+from database import insert_file
 
+def index_folder(folder):
+    for root, dirs, files in os.walk(folder):
+        for file in files:
+            path = os.path.join(root, file)
+            ext = os.path.splitext(file)[1]
+
+            insert_file(file.lower(), path, ext)
